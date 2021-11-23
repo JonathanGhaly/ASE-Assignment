9134040ef6321357d1f857a0342852d806d06d67
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2021-11-23 15:14:35 +0200

    added DB

--- soo2Werkab/src/DataRetriever.java
+++ soo2Werkab/src/DataRetriever.java
@@ -1,10 +1,144 @@
+import java.sql.*;
+
 public class DataRetriever {
+    Connection c = null;
+    Statement stmt;
     static DataRetriever dataRetriever;
 
-    private DataRetriever() {
+    public DataRetriever() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:soo2Werkab.db");
 
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
     }
-
+    public void AccountDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:soo2Werkab.db");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE Accounts " +
+                    "(IDAccount INTEGER PRIMARY KEY     NOT NULL," +
+                    " UserName       CHAR(50)    NOT NULL, " +
+                    " Password       CHAR(50)         NOT NULL, " +
+                    " Email          CHAR(50)  NULL , " +
+                    " mobileNo         CHAR(11) NOT NULL ,"+
+                    "isSuspended SMALLINT ,"+
+                    "create_time TEXT NULL )";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+    public void RidesDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:soo2Werkab.db");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE Rides " +
+                    "(IDRides INTEGER PRIMARY KEY     NOT NULL," +
+                    " SourceArea       CHAR(50)    NOT NULL, " +
+                    " DestinationArea  CHAR(50)         NOT NULL, " +
+                    " RideStatus          TEXT CHECK( RideStatus IN ('Pending','InRide','Completed') )   NOT NULL DEFAULT 'Pending' )";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+    public void UserAccountsDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:soo2Werkab.db");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE UserAccounts " +
+                    "(AccountID INTEGER ," +
+                    " UserStatus   TEXT CHECK( UserStatus IN ('Inactive','InRide','Pending','idle') )   NOT NULL DEFAULT 'idle'," +
+                    "FOREIGN KEY(AccountID)  REFERENCES Accounts(IDAccount))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+    public void driverAccountsDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:soo2Werkab.db");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE DriverAccount " +
+                    "(DriverID INTEGER ," +
+                    "LicenceNo CHAR(50) NOT NULL ,"+
+                    "IsVerified SMALLINT DEFAULT 0,"+
+                    "IsAccepted SMALLINT DEFAULT 0,"+
+                    " DriverStatus   TEXT CHECK( DriverStatus IN ('Inactive','InRide','Pending','idle') )   NOT NULL DEFAULT 'idle'," +
+                    "FOREIGN KEY(DriverID)  REFERENCES Accounts(IDAccount))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+    public void carDriverDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:soo2Werkab.db");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE CarDriver" +
+                    "(DriverID INTEGER ," +
+                    "LicenceNo CHAR(50) NOT NULL ,"+
+                    "Areas CHAR(50) NULL,"+
+                    "FOREIGN KEY(DriverID)  REFERENCES DriverAccount(DriverID))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+    public void RequestDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:soo2Werkab.db");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE Requests" +
+                    "(RequestID INTEGER PRIMARY KEY NOT NULL ," +
+                    "DriverID INTEGER ,"+
+                    "CustomerID INTEGER ,"+
+                    "RideID INTEGER ,"+
+                    "driverOffer DOUBLE NULL,"+
+                    "isAccepted SMALLINT NULL ,"+
+                    "FOREIGN KEY(DriverID)  REFERENCES DriverAccount(DriverID),"+
+                    "FOREIGN KEY(CustomerID) REFERENCES UserAccounts(AccountID),"+
+                    "FOREIGN KEY(RideID) REFERENCES Rides(IDRides))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
     static int getRating(Driver driver) {
         return 0;
     }
