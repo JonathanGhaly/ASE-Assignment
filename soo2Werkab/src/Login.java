5f8d8fb7d0dc9e30d69bfb9992f119b100766711
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2021-11-23 22:24:12 +0200

    Functions added in login and CarDriver

---
+++ soo2Werkab/src/Login.java
@@ -0,0 +1,11 @@
+public class Login {
+    DataRetriever d=DataRetriever.getInstance();
+    String username;
+    String password;
+    Login(String username,String password){
+        this.username=username;
+        this.password=password;
+        if(d.Login(this))
+            System.out.println("Logged in successfully");
+    }
+}
