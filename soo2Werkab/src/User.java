9dc8ff81552ebed5a15e3d1ac096385c80107d26
dovaaa <davidemadphilip@gmail.com>
2021-12-25 02:35:38 +0200

    ya hagry menak lelah

--- soo2Werkab/src/User.java
+++ soo2Werkab/src/User.java
@@ -1,13 +1,48 @@
+import java.util.ArrayList;
+
 public class User {
     Account account;
+    CarRequest currentRequest;
 
-
-    User(Account account) {
+    User(Account account){
         this.account = account;
     }
 
     public User() {
 
     }
+
+    void requestRide(Area source, Area destination) {
+        if (currentRequest == null)
+            currentRequest = new CarRequest(this, source, destination);
+    }
+
+    ArrayList<Offer> getOffers(){
+        DataRetriever db = DataRetriever.getInstance();
+        return db.getDriverOffer(this.currentRequest);
+    }
+
+    void acceptOffer(){
+
+    }
+    void rejectOffer(){
+
+    }
+
+    // TODO rate driver using driver operations.
+    void rateDriver(String driverUserName,Integer stars) {
+//        currentRequest.rate(stars);
+        DataRetriever db = DataRetriever.getInstance();
+        //Driver driver = db.getCarDriver(driverUserName);
+        //db.rateDriver(driver,stars);
+    }
+
+//
+//    String getDriverRating() {
+//        if (currentRequest != null)
+//            return currentRequest.getRating();
+//        return "You are not in a ride!";
+//    }
+
 
 }
