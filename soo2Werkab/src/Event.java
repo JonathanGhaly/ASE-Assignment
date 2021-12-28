678b9c095d4bb5acbd87f3c519385627300b2acc
dovaaa <davidemadphilip@gmail.com>
2021-12-28 22:10:15 +0200

    added logger and events class

--- soo2Werkab/src/Event.java
+++ soo2Werkab/src/Event.java
@@ -1,5 +1,6 @@
 public class Event {
     String eventInfo,eventSender,eventReceiver;
+
     Event(String eventName,Account sender,Account receiver){
     this.eventInfo=eventName;
     this.eventSender=sender.getUsername();
